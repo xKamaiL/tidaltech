ca5d8198b7dbf3e75cf4ba7303e28f7d7df9054e
Pakorn Sangpeth <camel4882@gmail.com>
2023-07-24 19:02:09 +0700

    wip: mode selection

--- apps/lib/pages/ligting/lighting.dart
+++ apps/lib/pages/ligting/lighting.dart
@@ -28,12 +28,50 @@ class LightingIndexPage extends HookConsumerWidget {
       backgroundColor: Colors.transparent,
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: n.Column(
-              [Panel(child: SpectrumCard()), Panel(child: TimeSelection())])
+          padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
+          child: n.Column([
+            ModeSelection(),
+            Panel(child: SpectrumCard()),
+            Panel(child: TimeSelection()),
+            Panel(child: TimeSelection()),
+          ])
             ..gap = 8,
         ),
       ),
     );
+  }
+}
+
+class ModeSelection extends StatelessWidget {
+  const ModeSelection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+          color: Colors.white.withOpacity(0.3),
+        ),
+        child: n.Row([
+          Expanded(
+            flex: 50,
+            child: n.Box(n.Text("Custom")..textAlign = TextAlign.center)
+              ..borderRadius = BorderRadius.circular(16.0)
+              ..bg = Colors.white.withOpacity(0.9),
+          ),
+          Expanded(
+            flex: 50,
+            child: GestureDetector(
+              child: n.Box(n.Text("Profile")..textAlign = TextAlign.center)
+                ..borderRadius = BorderRadius.circular(16.0)
+                ..bg = Colors.white.withOpacity(0.5),
+              onTap: () {},
+            ),
+          ),
+        ])
+          ..mx = 8
+          ..crossAxisAlignment = CrossAxisAlignment.center
+          ..crossAxisAlignment = CrossAxisAlignment.center
+          ..rounded = 16);
   }
 }
