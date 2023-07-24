c424bd113e6291b312299eb74cdbe083f53cb9a1
Pakorn Sangpeth <camel4882@gmail.com>
2023-07-24 19:36:04 +0700

    fix: design of selection

--- apps/lib/pages/ligting/lighting.dart
+++ apps/lib/pages/ligting/lighting.dart
@@ -13,6 +13,8 @@ class LightingIndexPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final mode = ref.watch(lightingModeProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lighting'),
@@ -52,82 +54,93 @@ class ModeSelection extends HookConsumerWidget {
     final mode = ref.watch(lightingModeProvider);
 
     return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.black.withOpacity(0.4),
+        ),
         child: n.Row([
-      Expanded(
-        child: GestureDetector(
-          child: SizedBox(
-            height: 37,
-            width: double.infinity,
-            child: AnimatedContainer(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(16),
-                  bottomLeft: Radius.circular(16),
+          Expanded(
+            child: GestureDetector(
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(left: 8.0, top: 6.0, bottom: 6.0),
+                child: SizedBox(
+                  height: 38,
+                  width: double.infinity,
+                  child: AnimatedContainer(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(8),
+                        bottomLeft: Radius.circular(8),
+                      ),
+                      color: Colors.black
+                          .withOpacity(mode == LightingMode.custom ? 0.4 : 0),
+                    ),
+                    duration: Duration(milliseconds: 250),
+                    child: Align(
+                      child: n.Row([
+                        n.Icon(Icons.edit)
+                          ..color = Colors.white
+                          ..size = 14.0,
+                        n.Text("Custom")
+                          ..textAlign = TextAlign.center
+                          ..color = Colors.white
+                      ])
+                        ..mainAxisAlignment = MainAxisAlignment.center
+                        ..gap = 4,
+                    ),
+                  ),
                 ),
-                color: Colors.black
-                    .withOpacity(mode == LightingMode.custom ? 0.4 : 0.2),
               ),
-              duration: Duration(milliseconds: 250),
-              child: Align(
-                child: n.Row([
-                  n.Icon(Icons.edit)
-                    ..color = Colors.white
-                    ..size = 14.0,
-                  n.Text("Custom")
-                    ..textAlign = TextAlign.center
-                    ..color = Colors.white
-                ])
-                  ..mainAxisAlignment = MainAxisAlignment.center
-                  ..gap = 4,
-              ),
+              onTap: () {
+                ref
+                    .read(lightingModeProvider.notifier)
+                    .setMode(LightingMode.custom);
+              },
             ),
           ),
-          onTap: () {
-            ref
-                .read(lightingModeProvider.notifier)
-                .setMode(LightingMode.custom);
-          },
-        ),
-      ),
-      Expanded(
-        child: GestureDetector(
-          child: SizedBox(
-            height: 37,
-            width: double.infinity,
-            child: AnimatedContainer(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(16),
-                  bottomRight: Radius.circular(16),
+          Expanded(
+            child: GestureDetector(
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(right: 8.0, top: 6.0, bottom: 6.0),
+                child: SizedBox(
+                  height: 38,
+                  width: double.infinity,
+                  child: AnimatedContainer(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(8),
+                        bottomRight: Radius.circular(8),
+                      ),
+                      color: Colors.black
+                          .withOpacity(mode == LightingMode.preset ? 0.4 : 0),
+                    ),
+                    duration: Duration(milliseconds: 250),
+                    child: Align(
+                      child: n.Row([
+                        n.Icon(Icons.list)
+                          ..color = Colors.white
+                          ..size = 16.0,
+                        n.Text("Preset")
+                          ..textAlign = TextAlign.center
+                          ..color = Colors.white
+                      ])
+                        ..mainAxisAlignment = MainAxisAlignment.center
+                        ..gap = 4,
+                    ),
+                  ),
                 ),
-                color: Colors.black
-                    .withOpacity(mode == LightingMode.preset ? 0.4 : 0.2),
               ),
-              duration: Duration(milliseconds: 250),
-              child: Align(
-                child: n.Row([
-                  n.Icon(Icons.list)
-                    ..color = Colors.white
-                    ..size = 16.0,
-                  n.Text("Preset")
-                    ..textAlign = TextAlign.center
-                    ..color = Colors.white
-                ])
-                  ..mainAxisAlignment = MainAxisAlignment.center
-                  ..gap = 4,
-              ),
+              onTap: () {
+                ref
+                    .read(lightingModeProvider.notifier)
+                    .setMode(LightingMode.preset);
+              },
             ),
           ),
-          onTap: () {
-            ref
-                .read(lightingModeProvider.notifier)
-                .setMode(LightingMode.preset);
-          },
-        ),
-      ),
-    ])
+        ])
           ..crossAxisAlignment = CrossAxisAlignment.center
-          ..crossAxisAlignment = CrossAxisAlignment.center
-          ..rounded = 16);
+          ..crossAxisAlignment = CrossAxisAlignment.center);
   }
 }
