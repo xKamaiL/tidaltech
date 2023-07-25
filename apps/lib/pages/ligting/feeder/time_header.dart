f25dcd91cd56e6e66efa8dd2e8d4186bcbdaef2b
Pakorn Sangpeth <camel4882@gmail.com>
2023-07-25 22:46:26 +0700

    Create time_header.dart

---
+++ apps/lib/pages/ligting/feeder/time_header.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:niku/namespace.dart' as n;
+import 'package:tidal_tech/theme/colors.dart';
+
+class FeederTimeHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return n.Row(List.generate(
+      7,
+      (index) => n.Text("${(index * 4).toString().padLeft(1, '0')}:00")
+        ..color = ThemeColors.foreground,
+    ))
+      ..spaceBetween;
+  }
+}
