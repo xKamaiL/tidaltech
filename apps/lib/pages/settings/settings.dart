9a030cb29066587fe75dc20980f4f7797b453d75
Pakorn Sangpeth <camel4882@gmail.com>
2023-07-27 23:35:36 +0700

    feat: setting appbar

--- apps/lib/pages/settings/settings.dart
+++ apps/lib/pages/settings/settings.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:tidal_tech/theme/colors.dart';
+import 'package:niku/namespace.dart' as n;
 
 class SettingsIndexPage extends HookConsumerWidget {
   const SettingsIndexPage({super.key});
@@ -8,9 +10,15 @@ class SettingsIndexPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Settings'),
+        title: n.Text("Settings")
+          ..color = Colors.grey.shade900
+          ..fontSize = 18.0
+          ..bold,
         elevation: 0,
         backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: true,
+        centerTitle: false,
+        actionsIconTheme: IconThemeData(color: Colors.grey.shade900),
       ),
       backgroundColor: Colors.transparent,
       body: Center(
