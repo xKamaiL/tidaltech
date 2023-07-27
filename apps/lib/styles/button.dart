afb0d0329744dd291a78c1a765c182fb34566a10
Pakorn Sangpeth <camel4882@gmail.com>
2023-07-28 01:08:10 +0700

    feat(button): danger style

--- apps/lib/styles/button.dart
+++ apps/lib/styles/button.dart
@@ -3,6 +3,28 @@ import 'package:niku/namespace.dart' as n;
 import 'package:tidal_tech/theme/colors.dart';
 
 class XButtonStyle {
+  static danger({
+    String label = "Delete",
+
+    bool? loading = false,
+    bool outline = false,
+
+    Icon? icon,
+  }) {
+    return n.Button.elevated("".n)
+      ..fontSize = 18
+      ..splash = ThemeColors.danger.withOpacity(0.1)
+      ..bg = ThemeColors.danger
+      ..px = 0
+      ..py = 16
+      ..elevation = 0
+      ..fontWeight = FontWeight.w600
+      ..color = Colors.white
+      ..icon = icon
+      ..label = label.n
+      ..rounded = 8;
+  }
+
   static confirm({
     bool loading = false,
     String label = "Confirm",
@@ -11,7 +33,8 @@ class XButtonStyle {
     return n.Button.elevated("".n)
       ..fontSize = 18
       ..splash = ThemeColors.primary.withOpacity(0.1)
-      ..bg = !loading ? ThemeColors.primary : ThemeColors.primary.withOpacity(0.5)
+      ..bg =
+          !loading ? ThemeColors.primary : ThemeColors.primary.withOpacity(0.5)
       ..px = 0
       ..py = 16
       ..elevation = 0
