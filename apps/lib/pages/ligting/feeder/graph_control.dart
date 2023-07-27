90d9901ee0272bf63dfc1cbb2e9a16c34fc9c3ac
Pakorn Sangpeth <camel4882@gmail.com>
2023-07-27 23:35:29 +0700

    feat: hide button when no select

--- apps/lib/pages/ligting/feeder/graph_control.dart
+++ apps/lib/pages/ligting/feeder/graph_control.dart
@@ -29,82 +29,85 @@ class TimeScheduleControl extends HookConsumerWidget {
             ..p = 0
             ..m = 0
             ..bg = ThemeColors.white,
-          n.Button(
-            n.Icon(Icons.remove)
-              ..color = selected != null
-                  ? ThemeColors.danger
+          if (selected != null)
+            n.Button(
+              n.Icon(Icons.remove)..color = ThemeColors.danger,
+            )
+              ..onPressed = () {
+                if (points.isEmpty) {
+                  return;
+                }
+                // open confirmation dialog
+                n.showNikuDialog(
+                  context: context,
+                  builder: (context) {
+                    return CupertinoAlertDialog(
+                      title: const Text("Delete time schedule point?"),
+                      content: const Text(
+                          "Are you sure you want to delete this time?"),
+                      actions: [
+                        CupertinoDialogAction(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Cancel"),
+                          isDestructiveAction: true,
+                        ),
+                        CupertinoDialogAction(
+                          onPressed: () {
+                            ref
+                                .read(timePointsNotifier.notifier)
+                                .delete(selected);
+                            ref
+                                .read(timePointEditingProvider.notifier)
+                                .remove();
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Delete"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              }
+              ..color = selected == null
+                  ? ThemeColors.mutedForeground
+                  : ThemeColors.foreground
+              ..rounded = 8
+              ..p = 0
+              ..m = 0
+              ..bg = selected != null
+                  ? ThemeColors.white
                   : ThemeColors.zinc.shade200.withOpacity(0),
-          )
-            ..onPressed = () {
-              if (points.isEmpty) {
-                return;
-              }
-              if (selected == null) {
-                return;
-              }
-              // open confirmation dialog
-              n.showNikuDialog(
-                context: context,
-                builder: (context) {
-                  return CupertinoAlertDialog(
-                    title: const Text("Delete time schedule point?"),
-                    content: const Text(
-                        "Are you sure you want to delete this time?"),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: Text("Cancel"),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          ref
-                              .read(timePointsNotifier.notifier)
-                              .delete(selected);
-                          ref.read(timePointEditingProvider.notifier).remove();
-                          Navigator.of(context).pop();
-                        },
-                        child: Text("Delete"),
-                      ),
-                    ],
-                  );
-                },
-              );
-            }
-            ..color = selected == null
-                ? ThemeColors.mutedForeground
-                : ThemeColors.foreground
-            ..rounded = 8
-            ..p = 0
-            ..m = 0
-            ..bg = selected != null
-                ? ThemeColors.white
-                : ThemeColors.zinc.shade200.withOpacity(0),
         ])
           ..gap = 4,
         n.Row([
-          n.Button(
-            n.Icon(Icons.play_arrow_outlined)..color = ThemeColors.foreground,
-          )
-            ..color = ThemeColors.foreground
-            ..rounded = 8
-            ..w = 60
-            ..p = 0
-            ..m = 0
-            ..bg = ThemeColors.white,
-          n.Button(
-            n.Icon(Icons.star_outline)..color = ThemeColors.foreground,
-          )
-            ..color = ThemeColors.foreground
-            ..rounded = 8
-            ..w = 40
-            ..p = 0
-            ..m = 0
-            ..bg = ThemeColors.white,
+          if (selected != null)
+            n.Button(
+              n.Icon(Icons.play_arrow_outlined)..color = ThemeColors.foreground,
+            )
+              ..color = ThemeColors.foreground
+              ..rounded = 8
+              ..w = 60
+              ..p = 0
+              ..m = 0
+              ..bg = ThemeColors.white,
+          if (selected != null)
+            n.Button(
+              n.Icon(Icons.star_outline)..color = ThemeColors.foreground,
+            )
+              ..color = ThemeColors.foreground
+              ..rounded = 8
+              ..w = 40
+              ..p = 0
+              ..m = 0
+              ..bg = ThemeColors.white,
           n.Button(
             n.Icon(Icons.menu)..color = ThemeColors.foreground,
           )
+            ..onPressed = () {
+              //
+            }
             ..color = ThemeColors.foreground
             ..rounded = 8
             ..w = 40
