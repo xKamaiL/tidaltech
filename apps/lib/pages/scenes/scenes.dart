0f5a03a5ed62f505de9313d9ae40cc4a82aff34d
Pakorn Sangpeth <camel4882@gmail.com>
2023-07-29 19:47:53 +0700

    feat: scenes

--- apps/lib/pages/scenes/scenes.dart
+++ apps/lib/pages/scenes/scenes.dart
@@ -11,42 +11,24 @@ class ScenesIndexPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Scenes'),
+        title: n.Text('Scenes and Effects')
+          ..color = Colors.grey.shade900
+          ..fontSize = 18.0
+          ..bold,
         elevation: 0,
         backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: true,
         centerTitle: false,
-      ),
-      backgroundColor: Colors.transparent,
-      body: ResponsiveGridList(
-        desiredItemWidth: 150,
-        minSpacing: 8,
-        children: [
-          Panel(
-            child: n.Row([
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.1),
-                  shape: BoxShape.circle,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: n.Icon(Icons.wb_sunny_outlined)
-                    ..color = Colors.white
-                    ..size = 24,
-                ),
-              ),
-              n.Column([n.Text('Morning')..color = Colors.white])..py = 12
-            ])
-              ..gap = 12
-              ..py = 4,
-          ),
-          Panel(
-            title: "Cloudy",
-            icon: Icons.cloud_outlined,
-            child: n.Text('Afternoon')..color = Colors.white,
+        actionsIconTheme: IconThemeData(color: Colors.grey.shade900),
+        actions: [
+          n.IconButton(
+            Icons.menu,
+            onPressed: () {},
           ),
         ],
       ),
+      backgroundColor: Colors.transparent,
+      body: n.Column([]),
     );
   }
 }
