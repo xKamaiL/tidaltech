4338f15a315dfa15d3659e8e965a523183f5d2b3
Pakorn Sangpeth <camel4882@gmail.com>
2023-08-08 19:30:18 +0700

    feat: app name

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -2,11 +2,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-
 void app_main(void)
 {
     int i = 0;
-    while (1) {
+    while (1)
+    {
         printf("[%d] Hello world!\n", i);
         i++;
         vTaskDelay(5000 / portTICK_PERIOD_MS);
