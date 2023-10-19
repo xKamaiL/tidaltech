e0679d8fa1166db1217fbb7dd1b56d3a13511d4c
premsomething2gether <s6401011610341@email.kmutnb.ac.th>
2023-10-19 17:16:58 +0700

    Create data from NVS

--- esp_idf/main/main.cpp
+++ esp_idf/main/main.cpp
@@ -12,9 +12,13 @@
 #include "freertos/timers.h"
 #include "proto/message.pb-c.h"
 #include "sdkconfig.h"
+#include "nvs_flash.h"
+#include "nvs.h"
+#define STORAGE_SCHEDULE "store"
 
 // LEDLevel
-typedef struct {
+typedef struct
+{
     uint32_t hh;
     uint32_t mm;
     uint32_t white;
@@ -27,59 +31,73 @@ typedef struct {
 } LEDLevel;
 
 // Schdule Item
-typedef struct {
+typedef struct
+{
     uint32_t hh;
     uint32_t mm;
     LEDLevel leds;
 } Schedule;
 
 /* Handler class for server events */
-class ServerCallbacks : public NimBLEServerCallbacks {
-    void onConnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo) {
+class ServerCallbacks : public NimBLEServerCallbacks
+{
+    void onConnect(NimBLEServer *pServer, NimBLEConnInfo &connInfo)
+    {
         printf("Client connected:: %s\n", connInfo.getAddress().toString().c_str());
     };
 
-    void onDisconnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo, int reason) {
+    void onDisconnect(NimBLEServer *pServer, NimBLEConnInfo &connInfo, int reason)
+    {
         printf("Client disconnected\n");
     };
 };
 
-void on_add_color_time_points(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo);
-void on_set_color_mode(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo);
-void on_set_ambient(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo);
+void on_add_color_time_points(NimBLECharacteristic *pCharacteristic, NimBLEConnInfo &connInfo);
+void on_set_color_mode(NimBLECharacteristic *pCharacteristic, NimBLEConnInfo &connInfo);
+void on_set_ambient(NimBLECharacteristic *pCharacteristic, NimBLEConnInfo &connInfo);
 
-class CharacteristicCallbacks : public NimBLECharacteristicCallbacks {
-    void onRead(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo) {
-        if (pCharacteristic->getUUID().equals(CHARACTERISTIC_UUID_GET_COLOR_MODE)) {
+class CharacteristicCallbacks : public NimBLECharacteristicCallbacks
+{
+    void onRead(NimBLECharacteristic *pCharacteristic, NimBLEConnInfo &connInfo)
+    {
+        if (pCharacteristic->getUUID().equals(CHARACTERISTIC_UUID_GET_COLOR_MODE))
+        {
             return;
         }
-        if (pCharacteristic->getUUID().equals(CHARACTERISTIC_UUID_GET_CURRENT_TIME)) {
+        if (pCharacteristic->getUUID().equals(CHARACTERISTIC_UUID_GET_CURRENT_TIME))
+        {
             return;
         }
-        if (pCharacteristic->getUUID().equals(CHARACTERISTIC_UUID_GET_COLOR_MODE)) {
+        if (pCharacteristic->getUUID().equals(CHARACTERISTIC_UUID_GET_COLOR_MODE))
+        {
             pCharacteristic->setValue(0);
             return;
         }
     };
 
-    void onWrite(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo) {
-        if (pCharacteristic->getUUID().equals(CHARACTERISTIC_UUID_ADD_COLOR_TIME_POINT)) {
+    void onWrite(NimBLECharacteristic *pCharacteristic, NimBLEConnInfo &connInfo)
+    {
+        if (pCharacteristic->getUUID().equals(CHARACTERISTIC_UUID_ADD_COLOR_TIME_POINT))
+        {
             on_add_color_time_points(pCharacteristic, connInfo);
             return;
         }
-        if (pCharacteristic->getUUID().equals(CHARACTERISTIC_UUID_SET_COLOR_MODE)) {
+        if (pCharacteristic->getUUID().equals(CHARACTERISTIC_UUID_SET_COLOR_MODE))
+        {
             on_set_color_mode(pCharacteristic, connInfo);
             return;
         }
-        if (pCharacteristic->getUUID().equals(CHARACTERISTIC_UUID_SET_AMBIENT)) {
+        if (pCharacteristic->getUUID().equals(CHARACTERISTIC_UUID_SET_AMBIENT))
+        {
             on_set_ambient(pCharacteristic, connInfo);
             return;
         }
     };
 
-    void onNotify(NimBLECharacteristic* pCharacteristic){};
+    void onNotify(NimBLECharacteristic *pCharacteristic){};
 
-    void onStatus(NimBLECharacteristic* pCharacteristic, int code) {
+    void onStatus(NimBLECharacteristic *pCharacteristic, int code)
+    {
         std::string str = ("Notification/Indication status code: ");
         str += ", return code: ";
         str += code;
@@ -88,17 +106,25 @@ class CharacteristicCallbacks : public NimBLECharacteristicCallbacks {
         printf(str.c_str());
     };
 
-    void onSubscribe(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo, uint16_t subValue) {
+    void onSubscribe(NimBLECharacteristic *pCharacteristic, NimBLEConnInfo &connInfo, uint16_t subValue)
+    {
         std::string str = "Client ID: ";
         str += " Address: ";
         str += std::string(connInfo.getAddress()).c_str();
-        if (subValue == 0) {
+        if (subValue == 0)
+        {
             str += " Unsubscribed to ";
-        } else if (subValue == 1) {
+        }
+        else if (subValue == 1)
+        {
             str += " Subscribed to notfications for ";
-        } else if (subValue == 2) {
+        }
+        else if (subValue == 2)
+        {
             str += " Subscribed to indications for ";
-        } else if (subValue == 3) {
+        }
+        else if (subValue == 3)
+        {
             str += " Subscribed to notifications and indications for ";
         }
         str += std::string(pCharacteristic->getUUID()).c_str();
@@ -107,37 +133,39 @@ class CharacteristicCallbacks : public NimBLECharacteristicCallbacks {
 };
 static CharacteristicCallbacks chrCallbacks;
 
-extern "C" {
-void app_main(void);
+extern "C"
+{
+    void app_main(void);
 }
 
-void app_main(void) {
+void app_main(void)
+{
     NimBLEDevice::init("TIDAL TECH LIGHTING");
     // NimBLEDevice::setPower(ESP_PWR_LVL_P9); /** +9db */
 
-    NimBLEServer* srv = NimBLEDevice::createServer();
+    NimBLEServer *srv = NimBLEDevice::createServer();
     srv->setCallbacks(new ServerCallbacks);
 
-    NimBLEService* deviceInformationService = srv->createService(DEVICE_INFORMATION_SERVICE_UUID);
+    NimBLEService *deviceInformationService = srv->createService(DEVICE_INFORMATION_SERVICE_UUID);
 
-    NimBLECharacteristic* deviceNameCharacteristic = deviceInformationService->createCharacteristic(
+    NimBLECharacteristic *deviceNameCharacteristic = deviceInformationService->createCharacteristic(
         CHARACTERISTIC_UUID_DEVICE_ID, NIMBLE_PROPERTY::READ);
     deviceNameCharacteristic->setValue(NimBLEUUID(""));
 
     deviceNameCharacteristic->setCallbacks(&chrCallbacks);
 
-    NimBLECharacteristic* deviceIdCharacteristic = deviceInformationService->createCharacteristic(
+    NimBLECharacteristic *deviceIdCharacteristic = deviceInformationService->createCharacteristic(
         CHARACTERISTIC_UUID_DEVICE_NAME, NIMBLE_PROPERTY::READ);
     deviceIdCharacteristic->setValue("TIDAL TECH LIGHTING");
     deviceIdCharacteristic->setCallbacks(&chrCallbacks);
 
-    NimBLEService* colorService = srv->createService(COLOR_SERVICE_UUID);
-    NimBLECharacteristic* getCurrentMode = colorService->createCharacteristic(CHARACTERISTIC_UUID_GET_COLOR_MODE, NIMBLE_PROPERTY::READ);
+    NimBLEService *colorService = srv->createService(COLOR_SERVICE_UUID);
+    NimBLECharacteristic *getCurrentMode = colorService->createCharacteristic(CHARACTERISTIC_UUID_GET_COLOR_MODE, NIMBLE_PROPERTY::READ);
     getCurrentMode->setCallbacks(&chrCallbacks);
-    NimBLECharacteristic* setColorMode = colorService->createCharacteristic(CHARACTERISTIC_UUID_SET_COLOR_MODE, NIMBLE_PROPERTY::WRITE_NR);
+    NimBLECharacteristic *setColorMode = colorService->createCharacteristic(CHARACTERISTIC_UUID_SET_COLOR_MODE, NIMBLE_PROPERTY::WRITE_NR);
     setColorMode->setCallbacks(&chrCallbacks);
 
-    NimBLECharacteristic* addColorTimePointCh = colorService->createCharacteristic(CHARACTERISTIC_UUID_ADD_COLOR_TIME_POINT, NIMBLE_PROPERTY::WRITE_NR);
+    NimBLECharacteristic *addColorTimePointCh = colorService->createCharacteristic(CHARACTERISTIC_UUID_ADD_COLOR_TIME_POINT, NIMBLE_PROPERTY::WRITE_NR);
     addColorTimePointCh->setCallbacks(&chrCallbacks);
 
     // get current mode
@@ -145,10 +173,10 @@ void app_main(void) {
     // get time point list
     // set mode (schedule, manual)
 
-    NimBLEService* rtcService = srv->createService(RTC_SERVICE_UUID);
+    NimBLEService *rtcService = srv->createService(RTC_SERVICE_UUID);
 
-    NimBLECharacteristic* getCurrentTime = rtcService->createCharacteristic(CHARACTERISTIC_UUID_GET_CURRENT_TIME, NIMBLE_PROPERTY::READ);
-    NimBLECharacteristic* setCurrentTime = rtcService->createCharacteristic(CHARACTERISTIC_UUID_SET_CURRENT_TIME, NIMBLE_PROPERTY::WRITE_NR);
+    NimBLECharacteristic *getCurrentTime = rtcService->createCharacteristic(CHARACTERISTIC_UUID_GET_CURRENT_TIME, NIMBLE_PROPERTY::READ);
+    NimBLECharacteristic *setCurrentTime = rtcService->createCharacteristic(CHARACTERISTIC_UUID_SET_CURRENT_TIME, NIMBLE_PROPERTY::WRITE_NR);
 
     getCurrentTime->setCallbacks(&chrCallbacks);
     setCurrentTime->setCallbacks(&chrCallbacks);
@@ -159,12 +187,12 @@ void app_main(void) {
     colorService->start();
     rtcService->start();
 
-    NimBLEAdvertising* pAdvertising = NimBLEDevice::getAdvertising();
+    NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
     pAdvertising->setName("TIDAL TECH LIGHTING");
     pAdvertising->addServiceUUID(deviceInformationService->getUUID());
     pAdvertising->addServiceUUID(colorService->getUUID());
     pAdvertising->addServiceUUID(rtcService->getUUID());
-    pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+    pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
     pAdvertising->setMinPreferred(0x12);
     pAdvertising->setScanResponse(true);
 
@@ -175,9 +203,11 @@ void app_main(void) {
     // FreeRTOS task
 }
 
-void on_add_color_time_points(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo) {
-    LightingScheduleRequest* req = lighting_schedule_request__unpack(NULL, pCharacteristic->getValue().length(), (uint8_t*)pCharacteristic->getValue().c_str());
-    if (req == NULL) {
+void on_add_color_time_points(NimBLECharacteristic *pCharacteristic, NimBLEConnInfo &connInfo)
+{
+    LightingScheduleRequest *req = lighting_schedule_request__unpack(NULL, pCharacteristic->getValue().length(), (uint8_t *)pCharacteristic->getValue().c_str());
+    if (req == NULL)
+    {
         printf("addColorTimePoint: decode message failed\n");
         return;
     }
@@ -195,13 +225,13 @@ void on_add_color_time_points(NimBLECharacteristic* pCharacteristic, NimBLEConnI
     leds.royal_blue = req->royal_blue;
     leds.ultra_violet = req->ultra_violet;
     lighting_schedule_request__free_unpacked(req, NULL);
-
-    // TODO: iterate through schedule or add new time point ?
 }
 
-void on_set_color_mode(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo) {
-    SetColorModeRequest* req = set_color_mode_request__unpack(NULL, pCharacteristic->getValue().length(), (uint8_t*)pCharacteristic->getValue().c_str());
-    if (req == NULL) {
+void on_set_color_mode(NimBLECharacteristic *pCharacteristic, NimBLEConnInfo &connInfo)
+{
+    SetColorModeRequest *req = set_color_mode_request__unpack(NULL, pCharacteristic->getValue().length(), (uint8_t *)pCharacteristic->getValue().c_str());
+    if (req == NULL)
+    {
         printf("onSetColorMode: decode message failed\n");
         return;
     }
@@ -214,9 +244,11 @@ void on_set_color_mode(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& co
     // TODO: change color mode ?
 }
 
-void on_set_ambient(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo) {
-    SetAmbientRequest* req = set_ambient_request__unpack(NULL, pCharacteristic->getValue().length(), (uint8_t*)pCharacteristic->getValue().c_str());
-    if (req == NULL) {
+void on_set_ambient(NimBLECharacteristic *pCharacteristic, NimBLEConnInfo &connInfo)
+{
+    SetAmbientRequest *req = set_ambient_request__unpack(NULL, pCharacteristic->getValue().length(), (uint8_t *)pCharacteristic->getValue().c_str());
+    if (req == NULL)
+    {
         printf("onSetAmbient: decode message failed\n");
         return;
     }
@@ -228,4 +260,29 @@ void on_set_ambient(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connI
     // TODO: change ambient R,G,B color
     // but do not change the current color mode
     printf("onSetAmbient: %d,%d,%d\n", r, g, b);
+}
+
+Schedule *read_schedule_from_nvs()
+{
+
+    nvs_handle_t handle;
+    esp_err_t err;
+
+    err = nvs_open(STORAGE_SCHEDULE, NVS_READONLY, &handle);
+    if (err != ESP_OK)
+        return NULL;
+
+    size_t max = 5;
+    Schedule *schedules = (Schedule *)malloc(max * sizeof(Schedule));
+    size_t size = sizeof(schedules);
+
+    err = nvs_get_blob(handle, "schedule", schedules, &size);
+
+    if (err != ESP_OK){
+        free(schedules);
+        return NULL;
+    }
+
+    nvs_close(handle);
+    return schedules;
 }
