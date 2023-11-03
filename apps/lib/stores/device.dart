07448d747e4e2826a010ceab3a6f2a0801e93f79
Pakorn Sangpeth <camel4882@gmail.com>
2023-11-03 15:45:43 +0700

    fix:

--- apps/lib/stores/device.dart
+++ apps/lib/stores/device.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tidal_tech/models/devices.dart';
 import 'package:tidal_tech/models/models.dart';
 
@@ -15,7 +16,9 @@ class DeviceNotifier extends StateNotifier<DeviceProvider> {
   DeviceNotifier(super.state);
 
   Future<void> fetchCurrentDevice() async {
+    final prefs = await SharedPreferences.getInstance();
     final res = await api.fetchDevice(PairParam(id: "id"));
+
     final xState = state;
     if (!res.ok) {
       if (res.error?.code == "DEVICE_NOT_FOUND") {
@@ -27,6 +30,7 @@ class DeviceNotifier extends StateNotifier<DeviceProvider> {
       state = xState;
       return;
     }
+    prefs.setString("deviceId", res.result!.id); //
     xState.isLoading = false;
     xState.isError = false;
     xState.isNotPair = false;
