6c3610df7a839e76843f4b23a5a616616ad551d8
Pakorn Sangpeth <camel4882@gmail.com>
2023-11-06 20:12:57 +0700

    Update led.h

--- esp_idf/main/led.h
+++ esp_idf/main/led.h
@@ -21,12 +21,12 @@
 // -- color led channel mapping --
 #define LED_WHITE LEDC_CHANNEL_0
 #define LED_WARM_WHITE LEDC_CHANNEL_1
-#define LED_RED LEDC_CHANNEL_2
-#define LED_GREEN LEDC_CHANNEL_3
-#define LED_BLUE LEDC_CHANNEL_4
-#define LED_ROYAL_BLUE LEDC_CHANNEL_5
-#define LED_ULTRA_VIOLET LEDC_CHANNEL_6
-#define LED_VIOLET LEDC_CHANNEL_7
+#define LED_RED LEDC_CHANNEL_5
+#define LED_GREEN LEDC_CHANNEL_3  // Blue
+#define LED_BLUE LEDC_CHANNEL_6   // UV
+#define LED_ROYAL_BLUE LEDC_CHANNEL_4
+#define LED_ULTRA_VIOLET LEDC_CHANNEL_7
+#define LED_VIOLET LEDC_CHANNEL_2
 
 void initialize_led();
 // led_display displays the current configuration of the LED
