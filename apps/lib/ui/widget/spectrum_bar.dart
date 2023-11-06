7130b965b2ec5ce8e7c23523defeedee14fc102a
Pakorn Sangpeth <camel4882@gmail.com>
2023-11-06 22:13:39 +0700

    chore: remove unused code

--- apps/lib/ui/widget/spectrum_bar.dart
+++ apps/lib/ui/widget/spectrum_bar.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:niku/namespace.dart' as n;
 import 'package:tidal_tech/providers/feeder.dart';
-import 'package:tidal_tech/providers/lighting.dart';
 import 'package:tidal_tech/theme/colors.dart';
 
 class Bar extends HookConsumerWidget {
@@ -54,63 +53,5 @@ class Bar extends HookConsumerWidget {
     );
 
     // random value
-  }
-}
-
-class RoundedRectangleSeekbarShape extends SliderComponentShape {
-  //The radius of the thumb
-  final double thumbRadius;
-
-  //the thickness of the border
-  final double thickness;
-
-  //the roundness of the corners
-  final double roundness;
-
-  RoundedRectangleSeekbarShape(
-      {this.thumbRadius = 0, this.thickness = 0, this.roundness = 6.0});
-
-  @override
-  Size getPreferredSize(bool isEnabled, bool isDiscrete) {
-    return Size.fromRadius(thumbRadius);
-  }
-
-  @override
-  void paint(PaintingContext context, Offset center,
-      {required Animation<double> activationAnimation,
-      required Animation<double> enableAnimation,
-      bool? isDiscrete,
-      required TextPainter labelPainter,
-      required RenderBox parentBox,
-      required SliderThemeData sliderTheme,
-      double? value,
-      double? textScaleFactor,
-      Size? sizeWithOverflow,
-      TextDirection? textDirection,
-      Thumb? thumb,
-      bool? isPressed}) {
-    final Canvas canvas = context.canvas;
-
-    final rect = Rect.fromCircle(center: center, radius: thumbRadius);
-
-    final roundedRectangle = RRect.fromRectAndRadius(
-      Rect.fromPoints(
-        Offset(rect.left - 1, rect.top),
-        Offset(rect.right + 1, rect.bottom),
-      ),
-      Radius.circular(roundness),
-    );
-
-    final fillPaint = Paint()
-      ..color = Colors.white
-      ..style = PaintingStyle.fill;
-
-    final borderPaint = Paint()
-      ..color = sliderTheme.thumbColor!
-      ..strokeWidth = thickness
-      ..style = PaintingStyle.stroke;
-
-    canvas.drawRRect(roundedRectangle, fillPaint);
-    canvas.drawRRect(roundedRectangle, borderPaint);
   }
 }
